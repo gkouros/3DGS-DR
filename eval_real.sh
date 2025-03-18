615d81a9593bc40f23b8d880371573b6c77a28c9
George Kouros <george.kouros.ece@gmail.com>
2025-03-18 10:18:47 +0100

    Add resolution argument

--- eval_real.sh
+++ eval_real.sh
@@ -1,3 +1,6 @@
-python -u eval.py -m logs/baseline/ref_real/gardenspheres --save_images
-python -u eval.py -m logs/baseline/ref_real/sedan --save_images
-python -u eval.py -m logs/baseline/ref_real/toycar --save_images
+#!/bin/bash
+set -e
+RESOLUTION=${1:-1}
+python -u eval.py -m logs/baseline_low_res/ref_real/gardenspheres -r ${RESOLUTION} --save_images
+python -u eval.py -m logs/baseline_low_res/ref_real/sedan         -r ${RESOLUTION} --save_images
+python -u eval.py -m logs/baseline_low_res/ref_real/toycar        -r ${RESOLUTION} --save_images
