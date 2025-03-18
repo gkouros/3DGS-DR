ee097674f6a125f8add75828726eabefc850cfcf
George Kouros <george.kouros.ece@gmail.com>
2025-03-18 12:46:00 +0100

    Fix path of experiments

--- eval_real.sh
+++ eval_real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 RESOLUTION=${1:-1}
-python -u eval.py -m logs/baseline_low_res/ref_real/gardenspheres -r ${RESOLUTION} --save_images
-python -u eval.py -m logs/baseline_low_res/ref_real/sedan         -r ${RESOLUTION} --save_images
-python -u eval.py -m logs/baseline_low_res/ref_real/toycar        -r ${RESOLUTION} --save_images
+python -u eval.py -m logs/baseline_res${RESOLUTION}/ref_real/gardenspheres -r ${RESOLUTION} --save_images
+python -u eval.py -m logs/baseline_res${RESOLUTION}/ref_real/sedan         -r ${RESOLUTION} --save_images
+python -u eval.py -m logs/baseline_res${RESOLUTION}/ref_real/toycar        -r ${RESOLUTION} --save_images
--- train_real.sh
+++ train_real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 RESOLUTION=${1:-1}
-python -u train.py -s data/ref_real/gardenspheres -m logs/baseline_low_res/ref_real/gardenspheres --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.2270 1.9700 1.7740 --env_scope_radius 0.974
-python -u train.py -s data/ref_real/sedan         -m logs/baseline_low_res/ref_real/sedan         --eval -r ${RESOLUTION} --iterations 61000 --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.032 0.808 0.751 --env_scope_radius 2.138
-python -u train.py -s data/ref_real/toycar        -m logs/baseline_low_res/ref_real/toycar        --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center 0.6810 0.8080 4.4550 --env_scope_radius 2.707
+python -u train.py -s data/ref_real/gardenspheres -m logs/baseline_res${RESOLUTION}/ref_real/gardenspheres --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.2270 1.9700 1.7740 --env_scope_radius 0.974
+python -u train.py -s data/ref_real/sedan         -m logs/baseline_res${RESOLUTION}/ref_real/sedan         --eval -r ${RESOLUTION} --iterations 61000 --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.032 0.808 0.751 --env_scope_radius 2.138
+python -u train.py -s data/ref_real/toycar        -m logs/baseline_res${RESOLUTION}/ref_real/toycar        --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center 0.6810 0.8080 4.4550 --env_scope_radius 2.707
