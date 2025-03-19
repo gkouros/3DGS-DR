f511879b56d173af8b286251d444b444dc3eff51
George Kouros <george.kouros.ece@gmail.com>
2025-03-19 13:03:15 +0100

    Add gui arg

--- train_real.sh
+++ train_real.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 RESOLUTION=${1:-1}
-python -u train.py -s data/ref_real/gardenspheres -m logs/baseline_res${RESOLUTION}/ref_real/gardenspheres --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.2270 1.9700 1.7740 --env_scope_radius 0.974
-python -u train.py -s data/ref_real/sedan         -m logs/baseline_res${RESOLUTION}/ref_real/sedan         --eval -r ${RESOLUTION} --iterations 61000 --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.032 0.808 0.751 --env_scope_radius 2.138
-python -u train.py -s data/ref_real/toycar        -m logs/baseline_res${RESOLUTION}/ref_real/toycar        --eval -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center 0.6810 0.8080 4.4550 --env_scope_radius 2.707
+GUI=${2:-}
+python -u train.py -s data/ref_real/gardenspheres -m logs/baseline_res${RESOLUTION}/ref_real/gardenspheres --eval ${GUI} -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.2270 1.9700 1.7740 --env_scope_radius 0.974
+python -u train.py -s data/ref_real/sedan         -m logs/baseline_res${RESOLUTION}/ref_real/sedan         --eval ${GUI} -r ${RESOLUTION} --iterations 61000 --longer_prop_iter 36_000 --use_env_scope --env_scope_center -0.032 0.808 0.751 --env_scope_radius 2.138
+python -u train.py -s data/ref_real/toycar        -m logs/baseline_res${RESOLUTION}/ref_real/toycar        --eval ${GUI} -r ${RESOLUTION} --iterations 61000  --longer_prop_iter 36_000 --use_env_scope --env_scope_center 0.6810 0.8080 4.4550 --env_scope_radius 2.707
