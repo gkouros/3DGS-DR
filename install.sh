27df172aeb4fe9b5037b06aa59280224f8c0a9ca
George Kouros <george.kouros.ece@gmail.com>
2025-05-23 15:10:43 +0200

    Add script that installs submodules

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export TORCH_CUDA_ARCH_LIST="6.0 7.0 7.5 8.0 8.6+PTX"
+export CUDA_HOME="$CONDA_PREFIX"
+export PATH="$CUDA_HOME/bin:$CONDA_PREFIX/bin:$PATH"
+export CPATH=$CONDA_PREFIX/targets/x86_64-linux/include:$CPATH
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib64/:$CONDA_PREFIX/bin/:$CONDA_PREFIX/lib/:$CONDA_PREFIX/lib/stub:$LD_LIBRARY_PATH"
+
+pip install submodules/cubemapencoder
+# pip install submodules/diff-gaussian-rasterization_c3
+# pip install submodules/diff-gaussian-rasterization_c7
+# pip install submodules/simple-knn
